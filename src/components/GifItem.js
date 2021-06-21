7ce88f31dd728e2d6484ba43a4bfde68964ccc5d
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-06-21 11:13:57 -0500

    animate css

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const GifItem = ({ url }) => {
   return (
-    <div className="card">
+    <div className="card animate__animated animate__backInLeft">
       <img src={url} alt="title" />
     </div>
   );
