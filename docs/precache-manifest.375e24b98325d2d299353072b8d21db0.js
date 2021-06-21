6bc829014663f820998738a88d35abfc0bc85f8f
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-06-21 11:18:18 -0500

    build implemented to github pages

---
+++ docs/precache-manifest.375e24b98325d2d299353072b8d21db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f18aa2ffd5918c603c2e28778f941f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e51579aec6fbc889d91",
+    "url": "/static/css/main.282f0ea9.chunk.css"
+  },
+  {
+    "revision": "56ef5bd83e665fec499f",
+    "url": "/static/js/2.57be7bb0.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.57be7bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e51579aec6fbc889d91",
+    "url": "/static/js/main.6492a49d.chunk.js"
+  },
+  {
+    "revision": "b34b5692cf0220b29cf7",
+    "url": "/static/js/runtime-main.d2bf2922.js"
+  }
+]);
